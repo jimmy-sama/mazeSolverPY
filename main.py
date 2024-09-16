675d6651e5e63306b958f3b06c0877b324922101
jimmy-sama <lungmuss@proton.me>
2024-09-16 08:53:08 +0200

    drawing cells complete

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
-from tkinter import Tk, BOTH, Canvas
+from graphics import Window
+from cell import Cell
 
-class Window:
-    def __init__(self, width, height):
-        self.__root = Tk()
-        self.__root.title("Maze Solver")
-        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
-        self.__canvas.pack(fill=BOTH, expand=1)
-        self.__running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
-    def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
+def main():
+    win = Window(800, 600)
 
-    def wait_for_close(self):
-        self.__running = True
-        while self.__running:
-            self.redraw()
-        print("window closed...")
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
 
-    def close(self):
-        self.__running = False
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 250, 250)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+
+    win.wait_for_close()
+
+main()
